f5aa942f067bb895639eeca8505872b9d087fa5a
Sangita-Dinda-21 <sangitadinda52@gmail.com>
2026-08-13 00:37:22 +0530

    Backend Setup

---
+++ backend/src/app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+
+import {createServer} from 'node:http';
+
+import {Server} from 'socket.io';
+
+import mongoose from 'mongoose';
+import connectToSocket from './controllers/SocketManager.js';
+
+import cors from 'cors';
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+
+app.set("port", (process.env.PORT || 8000));
+
+app.use(cors());
+
+app.use(express.json({limit:"40kb"}));
+
+app.use(express.urlencoded({limit:"40kb", extended:true}));
+
+const start = async () => {
+
+    const connectionDB = await mongoose.connect("mongodb+srv://sangita_dinda:dinda123@apnavidiocallcluster.9k2mhf9.mongodb.net/");
+
+    console.log(`MONGO Connected DB Host : ${connectionDB.connection.host}`);
+    app.listen(app.get("port"), ()=>{
+        console.log("Server is running on port " + app.get("port"));
+    })
+}
+
+start();
