d02464f1ea489cdf1e3d784151254aae17817c23
Sangita-Dinda-21 <sangitadinda52@gmail.com>
2026-08-17 00:46:36 +0530

    Landing page Setup Complete

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import '../App.css';
+import { Link } from 'react-router-dom';
+
+export default function LandingPage() {
+    return (
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>Apna Vidio Call</h2>
+                </div>
+                <div className = 'navlist'>
+                    <p>Join as Guest</p>
+                    <p>Register </p>
+                    <div role='button'>
+                        Login
+                    </div>
+                </div>
+            </nav>
+
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{color:"#FF9839"}}>Connect</span> with your loved ones</h1>
+                    <p>Cover a distance by Apna Vidio Call</p>
+                    <div role="button">
+                        <Link to={"/home"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+                    <img src = "/mobile.png"></img>
+                </div>
+            </div>
+
+
+
+
+
+
+        </div>
+    )
+}
